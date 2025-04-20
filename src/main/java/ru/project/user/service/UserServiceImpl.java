c9c7296f87b3603e4dedcbe457c30764b0ac4f0f
AveryanovaKatia <160598695+AveryanovaKatia@users.noreply.github.com>
2025-04-20 06:52:20 +0400

    добавить аутентификацию

--- src/main/java/ru/project/user/service/UserServiceImpl.java
+++ src/main/java/ru/project/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.project.user.dto.UserRequestDto;
 import ru.project.user.dto.UserResponseDto;
 import ru.project.user.mapper.UserMapper;
@@ -17,12 +18,16 @@ import ru.project.user.model.User;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto save(UserRequestDto userRequestDto) {
 
         log.info("Запрос на добавление пользователя");
-        final User saveUser = userRepository.save(UserMapper.toUser(userRequestDto));
+        final User newUser = UserMapper.toUser(userRequestDto);
+        newUser.setPassword(passwordEncoder.encode(userRequestDto.getPassword()));  // Хешируем пароль!
+
+        final User saveUser = userRepository.save(newUser);
 
         log.info("Пользователь успешно добавлен под id {}", saveUser.getId());
         return UserMapper.toUserResponseDto(saveUser);
